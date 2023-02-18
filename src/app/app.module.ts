c61b2a78284873c4bf8f96ec97cd4d9c92b82e20
Sebastián Miguel Castellano <smccba@gmail.com>
2023-02-18 05:53:12 -0300

    Commit 2
    Vamos adelantando un poco

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CVComponent } from './components/cv/cv.component';
+import { RSComponent } from './components/rs/rs.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CVComponent,
+    RSComponent,
+    BannerComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/rs/rs.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RSComponent } from './rs.component';
+
+describe('RSComponent', () => {
+  let component: RSComponent;
+  let fixture: ComponentFixture<RSComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RSComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(RSComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
